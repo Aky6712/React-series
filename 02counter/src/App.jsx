b62ff9a26aecea52f5e67034fb1b79c39ed884a5
Aky6712 <119603652+Aky6712@users.noreply.github.com>
2024-05-28 23:46:37 +0530

    counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(15) //setCounter or counter is just related name we can chage it its not by default
+
+  const addValue = () =>{
+    setCounter(counter + 1)
+  
+    
+  }
+
+  const removeValue = () =>{
+  if(counter>=1){
+    setCounter(counter-1)
+  }
+  }
+
+  
+  
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br/>
+      <br/>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
